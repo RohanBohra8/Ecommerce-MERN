e2b6f886ca275f84157210bb40e2ee361ad7d5c2
Rohan Bohra <rohanbohra8@gmail.com>
2023-08-11 13:19:15 +0530

    till user model, controller, hashing in authHelper

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:Number,
+        default:0
+    }
+},{timestamps:true}) //created time will be added when a new user is created
+
+export default mongoose.model('users',userSchema);

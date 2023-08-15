4cb71ffc79fff8b964f041549b99aeb7267d5991
Rohan Bohra <rohanbohra8@gmail.com>
2023-08-15 16:27:37 +0530

    created frontend and added footer using bootstrap

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        <h4 className='text-center'>
+          2023 All Rights Reserved &copy; RohanBohra
+        </h4>
+    </div>
+  )
+}
+
+export default Footer
